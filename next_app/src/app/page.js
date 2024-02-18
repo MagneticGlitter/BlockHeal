a14c280e09d2ddbe702a51ba449a9a77d6cf09c2
MagneticGlitter <92243872+MagneticGlitter@users.noreply.github.com>
2024-02-18 09:52:29 -0500

    1

--- next_app/src/app/page.js
+++ next_app/src/app/page.js
@@ -1,59 +1,14 @@
 "use client"
-process.env.AUTH0_SECRET= process.env.NEXT_PUBLIC_AUTH0_SECRET
-process.env.AUTH0_BASE_URL= process.env.NEXT_PUBLIC_AUTH0_BASE_URL
-process.env.AUTH0_ISSUER_BASE_URL= process.env.NEXT_PUBLIC_AUTH0_ISSUER_BASE_URL
-process.env.AUTH0_CLIENT_ID= process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID
-process.env.AUTH0_CLIENT_SECRET= process.env.NEXT_PUBLIC_AUTH0_CLIENT_SECRET
-
 import React, { useEffect, useState } from "react";
 import { useRouter } from 'next/navigation';
-import { useUser } from '@auth0/nextjs-auth0/client';
-// import { Main } from "./main"
-import Album from './Album'
 
 
 export default function Home() {
-  const {user, error , isLoading} = useUser()
 
   const router = useRouter();
 
-  const photos = [
-    { url: '1.jpg' },
-    { url: '2.jpg' },
-    { url: '3.jpg' },
-    { url: '4.jpg' },
-    { url: '5.jpg' },
-    { url: '6.jpg' },
-    { url: '7.jpg' },
-    { url: '8.jpg' },
-    { url: '9.jpg' },
-    // Add more photos as needed
-  ];
-
   const [list, setList] = useState([]);
-  
-  const handleFileUpload = (event) => {
-    const files = event.target.files; // Get the uploaded files
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-      // Construct a URL for the file
-      const url = URL.createObjectURL(file);
-      // Add the file to the list
-      setList(prevList => [...prevList, { url }]);
-    }
-  };
-
     
-  if (error) {
-    console.log(error.message)
-    return (
-      <div>
-        <p>This is an error message cuz user auth is causing an error</p>{error.message}
-      </div>
-    )
-  }
-
-  if (!user){
     return(
           // LANDING PAGE - PROJECT DESCRIPTION
     <div className="bg-white min-h-screen">
@@ -77,7 +32,7 @@ export default function Home() {
       <div className="mx-auto max-w-3xl py-16 sm:py-20 lg:py-24 z-10">
         <div className="text-center">
         <div class="flex justify-center items-center">
-            <img src="icon.png" alt="Icon" class="w-[100px] mb-4" /> {/* Adjust the size and margin as needed */}
+            <img src="icon.png" alt="Icon" class="w-[100px] mb-4 rounded-lg" /> {/* Adjust the size and margin as needed */}
         </div>
           <h1 className="text-3xl sm:text-5xl font-bold tracking-tight text-gray-900 sm:text-5xl mt-12 lg:mt-0">
             Welcome to BlockHeal.VR
@@ -88,41 +43,11 @@ export default function Home() {
           <div className="mt-12 flex items-center justify-center gap-x-6">
             <button
               className="rounded-md bg-blue-300 px-4 py-3 text-lg font-semibold text-black shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
-              ><a href="/api/auth/login">Log In</a></button>
+              ><a href="#">Get started</a></button>
           </div>
         </div>
       </div>
     </div>  
   </div>
-    )
-  } else if (isLoading){
-    return (
-      <div>loading</div>
-    )
-  } else{
-    return user && (
-      <div className="bg-blue-200 min-h-screen p-8 relative flex flex-col items-center justify-start">
-      <button className="absolute top-0 right-0 m-4 px-4 py-2 text-blue-800 border border-blue-800 rounded-md hover:bg-blue-800 hover:text-white transition duration-300"><a href="/api/auth/logout">Logout</a></button>
-      <img src="icon.png" alt="Icon" className="w-14 mb-4" /> {/* Adjust the size and margin as needed */}
-      <h1 className="text-3xl text-blue-800 font-bold mb-4">My Memory Album</h1>
-      <div className="w-80 flex flex-col items-center justify-center border-dashed border-2 border-blue-800 rounded-lg p-8 mb-8">
-          <input 
-              type="file" 
-              id="upload"
-              className="hidden" 
-              onChange={handleFileUpload}
-              multiple  // Allow multiple file selection
-          />
-          <label htmlFor="upload" className="cursor-pointer flex flex-col items-center">
-              <svg className="w-12 h-12 text-blue-800 mb-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
-              </svg>
-              <p className="text-blue-800">Upload Here!</p>
-          </label>
-      </div>
-      <Album photos={list} />
-  </div>
-  
-  ); 
-  }
+    );
 }
